d4bd2d7a808394af383b5b46f845f18d666c7408
Pouria Mirebrahimi <pouria.mirebrahimi@gmail.com>
2023-06-16 19:58:55 +0330

    Added user entity

---
+++ src/entity/user/user.entity.ts
@@ -0,0 +1,14 @@
+import { BaseEntity, Column, Entity } from 'typeorm';
+import { PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+}
